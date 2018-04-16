5499794f00a488de7f794a0b88222587c5bad794
dceoy <dnarsil+github@gmail.com>
2018-04-17 07:39:02 +0900

    Update tests

--- tests/build.sh
+++ tests/build.sh
@@ -7,8 +7,4 @@ cd $(dirname ${0})
 cp -a playbooks/* ../roles tmp
 cd tmp
 
-ls vars \
-  | cut -f 1 -d '.' \
-  | xargs -I {} cp vars/{}.yml roles/{}/vars/main.yml
-
 ansible-playbook test_provision.yml
