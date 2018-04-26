8ccc0949a9203c434d56333e8f7e2c047461b953
dceoy <dnarsil+github@gmail.com>
2018-04-26 23:46:53 +0900

    Update tests

--- tests/build.sh
+++ tests/build.sh
@@ -5,6 +5,9 @@ set -uex
 cd $(dirname ${0})
 [[ -d tmp ]] || mkdir tmp
 cp -a playbooks/* ../roles tmp
+for r in 'arm-debian' 'centos' 'docker' 'fedora' 'go' 'macos' 'python' 'r' 'ruby' 'ubuntu'; do
+  cp tmp/vars.yml tmp/roles/${r}/vars/main.yml
+done
 cd tmp
 
-ansible-playbook test_provision.yml
+ansible-playbook provision.yml
