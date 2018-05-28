2bd91c947077388104179f2c90749491793d04c0
dceoy <dnarsil+github@gmail.com>
2018-05-29 02:50:29 +0900

    Update tests

--- tests/build.sh
+++ tests/build.sh
@@ -5,7 +5,7 @@ set -uex
 cd $(dirname ${0})
 [[ -d tmp ]] || mkdir tmp
 cp -a playbooks/* ../roles tmp
-for r in 'arm-debian' 'centos' 'docker' 'fedora' 'go' 'macos' 'python' 'r' 'ruby' 'ubuntu'; do
+for r in 'arm-debian' 'centos' 'fedora' 'go' 'macos' 'python' 'r' 'ruby' 'ubuntu'; do
   cp tmp/vars.yml tmp/roles/${r}/vars/main.yml
 done
 cd tmp
